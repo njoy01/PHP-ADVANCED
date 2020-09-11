65e591bb5b5eccb07cc8b6e92eda6dd14423e16f
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-09-11 23:36:09 +0200

    PHP Advanced | taak: taak01

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,10 @@
+<?php 
+$Muhammad = array("Muhammad", "Burak", "17", "6A");
+echo 'voornaam= ' .$Muhammad[0];
+echo '<br> achternaam= ' .$Muhammad[1];
+echo '<br> leeftijd= ' .$Muhammad[2];
+echo '<br> klas= ' .$Muhammad[3];
+
+?>
+
+
