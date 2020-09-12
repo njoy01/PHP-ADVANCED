ad8a96bf25881cd39ab43fa367db45a0c2d8e94f
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-09-12 04:38:17 +0200

    PHP-Advanced-Loops-Taak-01 correcte versie

--- 2-Loops/taak01/images/loops-while.php
+++ 2-Loops/taak01/images/loops-while.php
@@ -1,9 +1,15 @@
 
 <?php
 $leeftijd = 0;
-while($leeftijd <= 17){
+while($leeftijd <= 16){
     $leeftijd++;
-    echo "ik ben   $leeftijd oud, dus ik heb stemrecht!". "<br>";
+    echo "ik ben $leeftijd jaar oud, dus ik heb geen stemrecht!". "<br>";
+}
+$leeftijd2 = 17;
+while($leeftijd2 >= 17){
+    $leeftijd2++;
+    echo "ik ben $leeftijd2 jaar oud, dus ik heb stemrecht!". "<br>";
+
 }
 ?>
 
